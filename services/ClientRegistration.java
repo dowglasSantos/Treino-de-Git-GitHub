7104cdd98f888e4d83e2d036ffa986d2427ba647
Dowglas <bittencourtdouglas41@gmail.com>
2025-01-02 20:24:36 -0300

    create folder services

--- services/ClientRegistration.java
+++ services/ClientRegistration.java
@@ -1,4 +1,4 @@
-package src;
+package services;
 
 public class ClientRegistration {
     public static void main(String[] args) {
--- services/ConectDataBase.java
+++ services/ConectDataBase.java
@@ -1,4 +1,4 @@
-package src;
+package services;
 
 public class ConectDataBase{
     public static void main(String[] args){
